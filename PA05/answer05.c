1dd167e0b0ad7fe1ebc4451e57fcd4b56e3cb259
lwelch <lwelch@purdue.edu>
2013-09-27 15:19:55 -0400

    I think everything is working. Just need to test and make sure that is correct.

---
+++ PA05/answer05.c
@@ -0,0 +1,389 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pa05.h"
+#define MAXIMUM_LENGTH 80
+
+/*
+ * Read a file of integers.
+ *
+ * Arguments:
+ *
+ * filename: the name of a file that contains a list of integers (one
+ * per line)
+ * 
+ * numInteger: pointer to store the number of integers in the
+ * file. You need to update the value stored at the address which is
+ * the pointer's value.  If the function fails to update the value, it
+ * is considered reading the file has failed.
+ *
+ * Returns:
+ *
+ * a array of integers from the file, or NULL if *any* error is
+ * encountered.
+ *
+ * Hint: use fscanf
+ *
+ * You do NOT know how many integers are in the file until you have
+ * read it. Once you know how many integers there are, you can modify
+ * the "numberOfIntegers" variable. (Note that this is a pointer, not
+ * an integer) You must allocate memory to store the integers.
+ * 
+ * Once memory is allocated to store the integers, you will need to
+ * re-read the values from the file. To do this, you must reset the
+ * file pointer to the beginning of the file using the function
+ * "fseek". 
+ *
+ * You should not use rewind (if you have learned it somewhere).  The
+ * difference of rewind and fseek is that rewind does not tell you
+ * whether it fails.  
+ *
+ * One way to read integeres is to use the "fscanf" function.  It is
+ * easier than reading one character at a time by using fgetc.
+ *
+ * You must use malloc in this function.
+ * 
+ * Some old books encourage readers to type cast in front of malloc,
+ * something like
+ *
+ * int * ptr = (int *) malloc(sizeof(int) * size);
+ *
+ * Type cast is no longer needed and in fact is discouraged now.  You
+ * should *NOT* type cast malloc.  It is discouraged even though it is
+ * not wrong.
+ *
+ * You will receive zero if you allocate a large array whose size is
+ * independent of the number of integers in the file.  For example, if
+ * you write this
+ *
+ * int array[10000];
+ * 
+ *
+ */
+
+int * readInteger(char * filename, int * numInteger)
+{
+  FILE * input; //Holds file array while searching for length
+  int placeholder; //placeholder for value compare when finding length and putting values in new array
+  int count2 = 0; //Keeps track of array index when inserting values
+  int * array; //array to store the contents of the opened file
+
+  input = fopen(filename, "r"); //initializes contact with file to be read
+  if (input == NULL) //Gives error if there is a problem connecting with the file
+    {
+      return NULL;
+    }
+
+  while(fscanf(input, "%d", &placeholder) != EOF) //Scans the file to get the length in order to produce an adaquete array length
+    {
+      * numInteger = *numInteger + 1; //Index of length of array in file
+    }
+  array = malloc((* numInteger) * sizeof(int)); //Allocates memory for the array to be made
+
+  fseek(input, 0 , SEEK_SET); //resets pointer of file back to index 0
+
+  while(count2 < * numInteger) //places elements of file into new array
+    {
+      fscanf(input, "%d", &placeholder);
+      array[count2]=placeholder;
+      count2++;
+    }
+  fclose(input); //closes communications with the file
+
+  return array; //returns the values in the array
+}
+
+/* ----------------------------------------------- */
+
+/*
+ * Read a file of strings. This is similar to readInteger. However,
+ * each string is an array of characters. The function needs to
+ * allocate memory for an array of strings, i.e., array of arrays of
+ * characters. In other words, this is a two-dimensional array (thus,
+ * char **).
+ *
+ * Arguments:
+ *
+ * filename: the name of a file that contains a list of strings (one
+ * per line)
+ * 
+ * numString: pointer to store the number of strings in the
+ * file. You need to update the value stored at the address which is
+ * the pointer's value.  If the function fails to update the value, it
+ * is considered reading the file has failed.
+ *
+ * Returns:
+ *
+ * a array of strings from the file, or NULL if *any* error is
+ * encountered.
+ *
+ * Hint: use fgets. 
+ * 
+ * Remember that an array of strings is a two-dimensional array of
+ * characters
+ *
+ * You do NOT know how many strings are in the file until you have
+ * read it. Once you know how many strings there are, you can modify
+ * the "numberOfStrings" variable. (Note that this is a pointer, not
+ * an string) You must allocate memory to store the strings.
+ * 
+ * Once memory is allocated to store the strings, you will need to
+ * re-read the values from the file. To do this, you must reset the
+ * file pointer to the beginning of the file using the function
+ * "fseek". 
+ *
+ * You should not use rewind (if you have learned it somewhere).  The
+ * difference of rewind and fseek is that rewind does not tell you
+ * whether it fails.  
+ *
+ * One way to read stringes is to use the "fscanf" function.  It is
+ * easier than reading one character at a time by using fgetc.
+ *
+ * You must use malloc in this function.
+ * 
+ * Some old books encourage readers to type cast in front of malloc,
+ * something like
+ *
+ * int * ptr = (int *) malloc(sizeof(int) * size);
+ *
+ * Type cast is no longer needed and in fact is discouraged now.  You
+ * should *NOT* type cast malloc.  It is discouraged even though it is
+ * not wrong.
+ *
+ * You will receive zero if you allocate a large array whose size is
+ * independent of the number of strings in the file.  For example, if
+ * you write this
+ *
+ * char array[10000];
+ * 
+ *
+ */
+
+char * * readString(char * filename, int * numString)
+{
+  FILE * input; //Holds file array while searching for length
+  int count;
+  int count2 = 0; //Keeps track of array index when inserting values
+  char buf[MAXIMUM_LENGTH];
+  char** array; //array to store the contents of the opened file
+  int count1 = 0;
+  
+  input = fopen(filename, "r"); //initializes contact with file to be read
+  if (input == NULL) //Gives error if there is a problem connecting with the file
+    {
+      return NULL;
+    }
+
+   while(fgets(buf, MAXIMUM_LENGTH, input) != NULL) //Scans the file to get the length in order to produce an adaquete array length
+    {
+      count1++; //Index of length of array in file
+    }
+   *numString = count1;
+    array = malloc((* numString) * sizeof(char*)); //Allocates memory for the array to be made
+    for(count = 0;count < *numString; count++)
+      {
+	array[count]=malloc(sizeof(char) * (MAXIMUM_LENGTH));
+      }
+
+     fseek(input, 0 , SEEK_SET); //resets pointer of file back to index 0
+
+  while(count2 < * numString) //places elements of file into new array
+    {
+      fgets(array[count2], MAXIMUM_LENGTH, input);
+      count2++;
+      }
+    fclose(input); //closes communications with the file
+    if(ferror(input)!=0)
+      {
+	return NULL;
+      }
+    
+  return array; //returns the values in the array
+}
+/* ----------------------------------------------- */
+/*
+ * print an array of integers, one integer per line
+ */
+void printInteger(int * arrInteger, int numInteger)
+{
+  int count;
+  for(count = 0; count < numInteger; count++)
+    {
+      printf("%i\n", arrInteger[count]);
+    }
+}
+
+/* ----------------------------------------------- */
+/*
+ * print an array of strings, one string per line
+ *
+ * Hint: printf("%s" ... can print a string
+ */
+void printString(char * * arrString, int numString)
+{
+  int count;
+  for(count = 0; count < numString; count++)
+    {
+      printf("%s", arrString[count]);
+    }
+
+}
+
+/* ----------------------------------------------- */
+/*
+ * release the memory occupied by the array of integers
+ */
+void freeInteger(int * arrInteger, int numInteger)
+{
+  free(arrInteger);
+}
+
+/* ----------------------------------------------- */
+/*
+ * release the memory occupied by the array of strings
+ *
+ * Hint: an array of strings is a two-dimensional array of characters
+ */
+void freeString(char * * arrString, int numString)
+{
+  int counter;
+  for(counter = 0; counter < numString; counter++)
+    {
+      free(arrString[counter]);
+    }
+  free(arrString);
+}
+
+/* ----------------------------------------------- */
+/*
+ * Write integers to a file, one integer per line
+ *
+ * Arguments:
+ *
+ * filename: the name of a file.
+ * arrInteger: an array of integers
+ * numInteger: the number of integers 
+ *
+ * Returns:
+ * 1 if saving to the file successfully
+ * 0 if *any* error is encountered
+ *
+ * Hint: use fprintf(... %d
+ *
+ */
+
+int saveInteger(char * filename, int * arrInteger, int numInteger)
+{
+  FILE * fout;
+  int counter;
+  fout = fopen(filename, "w");
+  for (counter = 0;counter < numInteger;counter++)
+    {
+      fprintf(fout, "%d\n", arrInteger[counter]);
+    }
+  fclose(fout);
+
+  if (ferror(fout) != 0);
+  {
+    return 0;
+  }
+  return 1;
+}
+
+/* ----------------------------------------------- */
+/*
+ * Write strings to a file, one string per line
+ *
+ * Arguments:
+ *
+ * filename: the name of a file.
+ * arrString: an array of strings
+ * numString: the number of strings 
+ *
+ * Returns:
+ * 1 if saving to the file successfully
+ * 0 if *any* error is encountered
+ *
+ * Hint: use fprintf(... %s
+ *
+ */
+
+int saveString(char * filename, char * * arrString, int numString)
+{
+  FILE * fout;
+  int counter;
+  fout = fopen(filename, "w");
+  for (counter = 0;counter < numString;counter++)
+    {
+      fprintf(fout, "%s", arrString[counter]);
+    }
+  fclose(fout);
+
+  if (ferror(fout) != 0);
+  {
+    return 0;
+  }
+  return 1;
+}
+
+/* ----------------------------------------------- */
+/*
+ * sort an arry of integers by calling the built-in qsort function in
+ * the C library.  You need to provide the comparison function. Please
+ * read the Linux manual about qsort
+ *
+ */
+
+int compint(const void *p1, const void *p2)
+{
+  int *intp1 = (int *)p1;
+  int *intp2 = (int *)p2;
+
+  int intv1 = *intp1;
+  int intv2 = *intp2;
+
+  if (intv1 < intv2)
+    {
+      return -1;
+    }
+  if (intv1 == intv2)
+    {
+      return 0;
+    }
+
+  return 1;
+}
+
+
+void sortInteger(int * arrInteger, int numInteger)
+{
+  qsort(& arrInteger[0],numInteger,sizeof(int),compint);
+
+}
+
+
+/* ----------------------------------------------- */
+/*
+ * sort an arry of strings by calling the built-in qsort function in
+ * the C library.  You need to provide the comparison function. Please
+ * read the Linux manual about qsort
+ *
+ * Hint: use strcmp in the comparison function
+ *
+ */
+int compstring(const void *p1, const void *p2)
+{
+  int compare;
+
+  compare=strcmp(*(char*const*)p1,*(char*const*)p2);
+
+  return compare;
+}
+
+
+void sortString(char * * arrString, int numString)
+{
+  qsort(& arrString[0],numString,sizeof(char**),compstring);
+}
+
+
